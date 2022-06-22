bb0764ab26614cf7dbec4c00f33a3a8060f73f4e
edtosoy <edberto.tosoy@gmail.com>
2022-06-22 19:40:23 +0800

    added fxnews callendar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/style-prop-object */
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={"font-size: 20px"} className="table-hieght">
+          © Powered by{" "}
+          <a
+            href="https://www.myfxbook.com"
+            class="myfxbookLink"
+            target="_self"
+            rel="noopener nofollow"
+          >
+            <strong>Myfxbook.com</strong>
+          </a>
+        </div>
       </header>
     </div>
   );
